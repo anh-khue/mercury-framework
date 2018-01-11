3296b090c70b08df28166174ac292e74b9bb4443
anh-khue <thailyanhkhue@gmail.com>
2018-01-11 17:52:27 +0700

    Clear master. Prepare for merging v.1.2.

--- src/app/repository/LecturerTeachSubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package app.repository;
-
-import app.model.LecturerTeachSubject;
-import framework.repository.CrudRepository;
-
-public class LecturerTeachSubjectRepository extends CrudRepository<LecturerTeachSubject> {
-}
