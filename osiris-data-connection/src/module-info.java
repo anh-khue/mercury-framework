7b6ff2e14e66d221cd1f2d02d91c1d68001aa43c
anh-khue <thailyanhkhue@gmail.com>
2018-01-25 22:57:22 +0700

    Remove old folders outsite osiris-data.

--- osiris-data-connection/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module osiris.data.connection {
-    requires transitive java.sql;
-    requires kotlin.stdlib;
-
-    exports io.osiris.data.connection;
-    exports io.osiris.data.connection.xml to osiris.data.jpa, osiris.data.repository;
-}
