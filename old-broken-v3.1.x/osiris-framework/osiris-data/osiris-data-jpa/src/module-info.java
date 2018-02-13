b2664007bc956ec8d3001737dd5e19352cfa4ae8
anh-khue <thailyanhkhue@gmail.com>
2018-02-13 16:11:19 +0700

    Remove old project's folder.

--- old-broken-v3.1.x/osiris-framework/osiris-data/osiris-data-jpa/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module osiris.data.jpa {
-    requires osiris.data.common;
-    requires osiris.data.connection;
-    requires java.sql;
-
-    exports io.osiris.data.jpa.binding to osiris.data.repository, osiris.query.common;
-    exports io.osiris.data.jpa;
-}
